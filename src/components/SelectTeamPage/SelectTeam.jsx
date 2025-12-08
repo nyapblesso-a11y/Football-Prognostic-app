fd2b1df71f08f6583841823f7fc32f20a74dbb54
nyapblesso-a11y <nyapblesso@gmail.com>
2025-12-08 16:56:05 +0100

    data has been dsiplayed on the select-team-page

--- src/components/SelectTeamPage/SelectTeam.jsx
+++ src/components/SelectTeamPage/SelectTeam.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import data from "../../data.json";
-import "./SelecTeamPage.css"
+import "./SelecTeamPage.css";
 function SelectTeam() {
   return (
     <>
-        <div className="select-section">
-          <h1>Various teams</h1>
-          <ul className="card">
-            {data.clubs.map((club, index) => (
-              <li key={index} className="teams">
-                <img src={club.url} alt="" />
-                <span>{club.name}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-     
+      <div className="select-section">
+        <h1>Various teams</h1>
+        <ul className="card">
+          {data.clubs.map((club, index) => (
+            <li key={index} className="teams">
+              <img src={club.url} alt="" />
+              <span>{club.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
