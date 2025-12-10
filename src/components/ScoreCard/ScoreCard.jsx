321a521728260bfdd319c17de17d4ec99fc0db5d
nyapblesso-a11y <nyapblesso@gmail.com>
2025-12-10 16:11:47 +0100

    the download component implemented

--- src/components/ScoreCard/ScoreCard.jsx
+++ src/components/ScoreCard/ScoreCard.jsx
@@ -6,13 +6,12 @@ import { FaDownload, FaPlus, FaMinus } from "react-icons/fa";
 function ScoreCard() {
   const [scoreTeam1, setScoreTeam1] = useState(0);
   const [scoreTeam2, setScoreTeam2] = useState(0);
+  const ScoreCardRef = useRef(null);
 
   const incrementScore1 = () => setScoreTeam1((prev) => prev + 1);
   const decrementScore1 = () => setScoreTeam1((prev) => Math.max(0, prev - 1));
   const incrementScore2 = () => setScoreTeam2((prev) => prev + 1);
   const decrementScore2 = () => setScoreTeam2((prev) => Math.max(0, prev - 1));
-
-  const ScoreCardRef = useRef(null);
 
   const handleDownload = async () => {
     const element = ScoreCardRef.current;
@@ -39,7 +38,7 @@ function ScoreCard() {
     <>
       <div ref={ScoreCardRef} className="score">
         <div className="header">
-          <h1>Football-Score Predictor</h1>
+          <h1>Football-Score Predicts</h1>
         </div>
         <div className="score-card">
           <div className="team-1">
